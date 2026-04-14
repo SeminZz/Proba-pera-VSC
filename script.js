ed7fcd29e55530ce86c5a632cbf061feb4d19dbd
SeminZz <dany2717@gmail.com>
2026-04-14 14:52:42 +0300

    Правка Имя

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const welcomeMessage = document.getElementById('welcomeMessage');
 greetButton.addEventListener('click', () => {
     const name = nameInput.value;
     if (name) {
-        welcomeMessage.textContent = `Шалом, ${name}! Мы рады видеть тебя, скинь денег на карту по номеру - +7 (999) 888-12-34`;
+        welcomeMessage.textContent = `Шалом, ${name}! Мы рады видеть тебя, скинь денег на карту по номеру +7 (978) 881-69-28`;
         nameInput.value = ''; // Очистить поле после нажатия
     } else {
-        alert('Пожалуйста, введи свое имя');
+        alert('Пожалуйста, введи свое имя, ну или не имя');
     }
 });
